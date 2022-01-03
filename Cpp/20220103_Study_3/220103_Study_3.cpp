6baeb392932e889855460c3feb6aeefd10eacff4
Charles <jackpar@nate.com>
2022-01-03 22:41:40 +0900

    20220103_Study_3

---
+++ Cpp/20220103_Study_3/220103_Study_3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+class Character {
+private:
+	int _health;
+	int _power;
+
+public: 
+	Character(int health, int power) :_health(health), _power(power) {
+
+	}
+
+	virtual void damaged(int power) {
+		_health -= power;
+	}
+
+	void attack(Character& target) const {
+		target.damaged(_power);
+	}
+};
+
+class Player : public Character {
+public:
+	Player(int health, int power) :Character(health, power) {
+
+	}
+
+	virtual void damaged(int power) override {
+		Character::damaged(power);
+		cout << "À¸¾Ç" << endl;
+	}
+};
+
+class Monster : public Character {
+public:
+	using Character::Character;
+
+	virtual void damaged(int power ) override {
+		Character::damaged(power);
+		cout << "²Ï¾×" << endl;
+	}
+};
+int main() {
+	Player player(200, 100);
+	Monster monster(100, 50);
+
+	player.attack(monster);
+	monster.attack(player);
+}
